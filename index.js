ed36333532c3f5ab39d2b84b09518d6693c778cf
Pradipat <P0861719933@gmail.com>
2024-01-24 22:49:37 +0700

    5Page finished

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// เปิดปิดแถบเมนูขวาบน
 document.addEventListener('DOMContentLoaded', function () {
     const menuButton = document.querySelector('.menu-buttom');
     const openedMenuContaine = document.querySelector('.opened-menu-container');
@@ -7,3 +8,22 @@ document.addEventListener('DOMContentLoaded', function () {
         openedMenuContaine.classList.toggle('menu-visible');
     });
 });
+
+
+function toggleDropdown(dropdownId) {
+    var dropdown = document.getElementById(dropdownId);
+    dropdown.classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
